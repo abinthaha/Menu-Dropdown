f53e7745133679f5d3be49e9f91052cf05702e53
Abin Thaha <abin@qburst.com>
2016-11-01 11:52:00 +0530

    Menu complete

---
+++ js/script.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+    // $('.menu-wrapper .menu-item').hover(function(event){
+    //     event.stopPropagation();
+    //     var menuItem = $(this).closest('.menu-item'),
+    //         dropdownMenu = menuItem.find('.menu-item-dropdown');
+    //
+    //     $(dropdownMenu).fadeIn("fast");
+    // });
+
+    // $('.menu-wrapper .menu-item-name').mouseout(function(event){
+    //     event.stopPropagation();
+    //     var menuItem = $(this).closest('.menu-item'),
+    //         dropdownMenu = menuItem.find('.menu-item-dropdown');
+    //
+    //     $(dropdownMenu).fadeOut("slow");
+    // });
+    // $('.menu-item-dropdown').hover(function(event) {
+    //     event.stopPropagation();
+    //     $(this).show();
+    // });
+    // $('.menu-item-dropdown').mouseout(function(event) {
+    //     event.stopPropagation();
+    //     $(this).hide();
+    // });
+});
